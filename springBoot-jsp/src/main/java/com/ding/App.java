187e6d2a98fe8cc99f8b8797c7eaffc40cf39ed6
ding <dingyaqiang@100msh.com>
2016-07-07 16:26:56 +0800

    update

--- springBoot-jsp/src/main/java/com/ding/App.java
+++ springBoot-jsp/src/main/java/com/ding/App.java
@@ -1,13 +1,9 @@
 package com.ding;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
